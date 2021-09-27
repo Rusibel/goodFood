1f80f90679890dddd177d5cd8ce054dbd4803c3d
sibel <sibel10789@gmail.com>
2021-09-27 16:26:05 +0300

    060 slider-1

--- js/script.js
+++ js/script.js
@@ -436,6 +436,108 @@ window.addEventListener('DOMContentLoaded', () => {
     //     .then(data => data.json())
     //     .then(res => console.log(res));
 
+    //Slider - мой вариант
+
+    const prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          current = document.querySelector('#current'),
+          total = document.querySelector('#total'),
+          slides = document.querySelectorAll('.offer__slide');
+
+    console.log(slides);
+
+    total.textContent = getZero(slides.length);
 
 
+    function hideSlides() {
+        slides.forEach((item, i) => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+    }
+    let currentSlide = 0;
+    function showSlides(i) {
+        slides[i].classList.add('show', 'fade');
+        slides[i].classList.remove('hide');
+        current.textContent = getZero(i + 1);
+    }
+    function slider(currentSlide){
+        hideSlides();
+        showSlides(currentSlide);
+    }
+
+    slider(currentSlide);
+    
+    console.log(currentSlide);
+
+    next.addEventListener('click', () => {
+            currentSlide++;
+            if (currentSlide < slides.length){                
+                slider(currentSlide);
+            }else{
+                currentSlide = 0;
+                slider(currentSlide);                
+            }
+    });
+
+    prev.addEventListener('click', () => {
+            currentSlide--;
+        if (currentSlide >= 0 && currentSlide < slides.length){
+            slider(currentSlide);
+        }else{
+            currentSlide = slides.length - 1;
+            slider(currentSlide);
+            console.log(currentSlide);
+        }
+
+    });
+
+    // Slider - ответы
+
+    // let slideIndex = 1;
+    // const slides = document.querySelectorAll('.offer__slide'),
+    //     prev = document.querySelector('.offer__slider-prev'),
+    //     next = document.querySelector('.offer__slider-next'),
+    //     total = document.querySelector('#total'),
+    //     current = document.querySelector('#current');
+
+    // showSlides(slideIndex);
+
+    // if (slides.length < 10) {
+    //     total.textContent = `0${slides.length}`;
+    // } else {
+    //     total.textContent = slides.length;
+    // }
+
+    // function showSlides(n) {
+    //     if (n > slides.length) {
+    //         slideIndex = 1;
+    //     }
+    //     if (n < 1) {
+    //         slideIndex = slides.length;
+    //     }
+
+    //     slides.forEach((item) => item.style.display = 'none');
+
+    //     slides[slideIndex - 1].style.display = 'block'; // Как ваша самостоятельная работа - переписать на использование классов show/hide
+        
+    //     if (slides.length < 10) {
+    //         current.textContent =  `0${slideIndex}`;
+    //     } else {
+    //         current.textContent =  slideIndex;
+    //     }
+    // }
+
+    // function plusSlides (n) {
+    //     showSlides(slideIndex += n);
+    // }
+
+    // prev.addEventListener('click', function(){
+    //     plusSlides(-1);
+    // });
+
+    // next.addEventListener('click', function(){
+    //     plusSlides(1);
+    // });
 });
+
